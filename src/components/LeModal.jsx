348357862ce7abdb187e23c21817c4b5ff40880b
Hameet <hameet.singh@hotmail.com>
2018-05-31 15:57:48 +1200

    is modal responsive

--- src/components/LeModal.jsx
+++ src/components/LeModal.jsx
@@ -1,59 +1,41 @@
 import React, { Component } from 'react'
-import ReactModal from 'react-modal'
+import Modal from 'react-responsive-modal'
 import { Button, Col } from 'react-bootstrap';
 import './LeModal.css'
 
 
-const style = {
-  content: {
-    border: '0',
-    borderRadius: '10px',
-    bottom: '35%',
-    height: '200px',  // set height
-    left: '30%',
-    padding: '20rem',
-    position: 'fixed',
-    right: '50%',
-    top: '30%', // start from center
-    transform: 'translate(-90%,-' + ')', // adjust top "up" based on height
-    width: '60%',
-    maxWidth: '80rem',
-    overflow: 'none',
-    background: 'palevioletred'
-  }
+const modalStyle = {
+  fontFamily: "sans-serif",
+  textAlign: "left",
+  color: "green",
+  width: "90%"
 };
 
 export default class LeModal extends React.Component {
-  constructor () {
-    super();
+  constructor(props) {
+    super(props);
+    this.onOpenModal = this.onOpenModal.bind(this);
+    this.onCloseModal = this.onCloseModal.bind(this);
     this.state = {
-      showModal: false
+      open: false,
     };
-    
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
-  
-  handleOpenModal () {
-    this.setState({ showModal: true });
+ 
+  onOpenModal() {
+    this.setState({ open: true });
   }
-  
-  handleCloseModal () {
-    this.setState({ showModal: false });
+ 
+  onCloseModal() {
+    this.setState({ open: false });
   }
-  
-  render () {
+ 
+  render() {
+    const { open } = this.state;
     return (
-      <div className="modal-container">
-        <strong><button onClick={this.handleOpenModal}>Contact Info</button></strong>
-        <ReactModal 
-           isOpen={this.state.showModal}
-           style={style}
-           contentLabel="onRequestClose Example"
-           onRequestClose={this.handleCloseModal}
-           shouldCloseOnOverlayClick={false}
-        >
-          <Col xs={3} md={6}>
+      <div style={modalStyle}>
+        <button onClick={this.onOpenModal}>Open modal</button>
+        <Modal open={open} onClose={this.onCloseModal} little style={modalStyle} >
+          
           <div className="modal-header">
           
           <h5>Photos courtesty of
@@ -62,16 +44,12 @@ export default class LeModal extends React.Component {
           </div>
           
           <div className="modal-footer">
-          <h5>Contact</h5>
+          <h5>Contact
           <a href="https://github.com/Hameet" target="_blank" className="fa fa-github"></a> <br/>
-          <a href="https://www.linkedin.com/in/hameet-boparai-79951827/" target="_blank" className="fa fa-linkedin"></a>
+          <a href="https://www.linkedin.com/in/hameet-boparai-79951827/" target="_blank" className="fa fa-linkedin"></a></h5>
          
           </div>
-          </Col>
-
-          <strong><button onClick={this.handleCloseModal}>Close Modal</button></strong>
-          
-        </ReactModal>
+        </Modal>
       </div>
     );
   }
