64bc5cf11fbb0b8ed0d1111c00a40482693a76ce
Hameet <hameet.singh@hotmail.com>
2018-06-03 23:30:20 +1200

    new content

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
-import { Grid, Col, Image } from 'react-bootstrap';
+import { Grid, Col, Image, Row } from 'react-bootstrap';
 import './About.css';
 
 export default class About extends Component {
   render() {
     return (
-      <div>
-        {/* <Image src="assets/dog-people.jpg" className="header-image" /> */}
+      <div className="aboutme">
         <Grid>
-          <Col xs={12} sm={8} smOffset={2}>
-            {/* <Image src="assets/person-1.jpg" className="about-profile-pic" rounded /> */}
-            <h3>Content coming</h3>
-            
-          </Col>
+          <Row className="background">
+            <h1>Some Background</h1>
+            <p text="justify">I've several years of experience working in Food and Drug testing for NZ and multinational companies. I was always passionate about science and took great pride in my work but technology has always been my first love. It took a few years of soul searching but once I started learning how to code I knew it was what I wanted to do.</p>
+          </Row>
+
+          <Row className="background">
+          <h1>What do I like about code?</h1>
+          <p>I like the satisfaction that you get from creating something. There are so many new technologies emerging and I would love to be a part of something bigger which will shape people's lives. Although I like the server side stuff, I belive my real strength is in Design and Front end. I'm intersted in learning technologies such as AI, Augmented Reality, Data science and machine learning etc and to broaden my horizons.</p>
+          </Row>
+
+          <Row className="background">
+          <h1>Why code?</h1>
+          <p>I've tried my hands at learning and aquiring other skills such as learning a new language (tried Spanish but gave up) or learn how to play the guitar (felt very cliche) but nothing stuck. However, with coding my interset grew and grew and I was lucky find an Institue like Enspiral Dev Academy. I'm far from an expert but definietly am improving everyday.</p>
+          </Row>
         </Grid>
       </div>
     )
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -6,15 +6,44 @@ export default class News extends Component {
   render() {
     return (
       <div>
-        <Image src="assets/mountain-man.jpg" className="header-image" />
-        <Grid>
-          <h2>News</h2>
-          <Row>
-            <Col xs={12} sm={8} className="main-section">
+        <h1>Content Coming Soon</h1>
+        {/* <Grid>
+
+        <Row className="background">
+            <h1>Some of my Phase-0 projects I made in my early days of coding</h1>
+        </Row>
+
+            <Row>
+            <Col xs={6} md={9} mdPull={2}  className="main-section">
+            <h6>Animated circles</h6>
+            <div class="animation">
+            <a href="https://hameet.github.io/sprint%209/index.html"><Image src="assets/circles.gif"/></a>
+            <h9>A small animation I made with some Daft Punk playing in the backgroundClick me</h9>
+            </div>
+           </Col>
+           </Row>
+
+           <Row>
+           
+           <div class="game-container">
+           <h11>Calculator & Minesweeper</h11>
+             <Col md={3} mdPull= {3}>
+                             
+             <a href="https://hameet.github.io/js-calculator/"><Image src="assets/calculator.gif"/></a>
+                  <h9>Need some help calculating your Finances Click me</h9>
+                
+                </Col>
+            
+              <Col  md={3} mdOffset={3}>
               
-            </Col>
+              <a href="https://hameet.github.io/js-calculator/"><Image src="assets/minesweeper.gif" className="bomb"/></a>
+                  <h9>IGN gave it 10/10 Click me</h9>
+                
+              </Col>
+           
+              </div>
           </Row>
-        </Grid>
+        </Grid> */}
       </div>
     )
   }
