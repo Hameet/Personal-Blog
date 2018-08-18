eee63d303dbb8a4e4c4d253177dfd6f844834ebb
Hameet <hameet.singh@hotmail.com>
2018-08-18 19:42:21 +1200

    boot

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -17,7 +17,7 @@ export default class News extends Component {
             <Col xs={6} md={9} mdPull={2} className="main-section">
               <Image src="assets/circles.gif" className="circles" />
               <div class="animation">
-                <a href="https://hameet.github.io/sprint%209/index.html"><h9>A small animation I made with some Daft Punk playing in the background</h9></a>
+                <a className="project-link" href="https://hameet.github.io/sprint%209/index.html"><h9>A small animation I made with some Daft Punk playing in the background</h9></a>
               </div>
             </Col>
           </Row>
@@ -28,7 +28,7 @@ export default class News extends Component {
             <div className="calc-container">
               <Image src="assets/calculator.gif" className="calc" />
                 <div className="calculator">
-                  <a href="https://hameet.github.io/js-calculator/"><h11>Need some help calculating your Finances</h11></a>
+                  <a className="project-link" href="https://hameet.github.io/js-calculator/"><h11>Need some help calculating your Finances</h11></a>
                 </div>
             </div>
           </Col>
@@ -37,7 +37,7 @@ export default class News extends Component {
             <div className="bomb-container">
               <Image src="assets/minesweeper.gif" className="bomb" />
                 <div className="mine">
-                  <a href="https://hameet.github.io/minesweeper/"><h10>IGN gave it 10/10</h10></a>
+                  <a className="project-link" href="https://hameet.github.io/minesweeper/"><h10>IGN gave it 10/10</h10></a>
                 </div>
             </div>
           </Col>
